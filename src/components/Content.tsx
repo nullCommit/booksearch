cefbf0e01ada1f81e7bcf9678d8e28b19da8fbcc
Allan <allangeek@gmail.com>
2022-05-31 17:36:03 -0300

    Update: add book image when there are no search result

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,6 +1,8 @@
 import { useSearch } from '../hooks/useSearch';
 import { ContentCard } from './ContentCard';
 
+import openedBookImage from '../assets/opened-book.svg';
+
 export function Content() {
   const { searchResults, loadNextResults } = useSearch();
 
@@ -11,13 +13,18 @@ export function Content() {
           <ContentCard key={result.id} content={result.volumeInfo} />
         ))}
 
-      {searchResults && (
+      {searchResults.length > 0 ? (
         <button
           onClick={loadNextResults}
           className='my-7 text-brand-500 font-semibold '
         >
           Carregar mais resultados
         </button>
+      ) : (
+        <div className='flex flex-col items-center mt-48'>
+          <img src={openedBookImage} className='opacity-20'></img>
+          <span>Search for one book</span>
+        </div>
       )}
     </div>
   );
