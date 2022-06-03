dc623d0799cd07a1ab6ee2a8399acce9361e6ecb
Allan <allangeek@gmail.com>
2022-06-03 14:29:50 -0300

    Update: add responsivity to ContentCards component

--- src/components/ContentCard.tsx
+++ src/components/ContentCard.tsx
@@ -15,39 +15,45 @@ interface ContentCardProps {
 
 export function ContentCard({ content }: ContentCardProps) {
   return (
-    <div className='w-[43rem] h-36 flex items-center relative gap-3 my-4 overflow-hidden'>
+    <div className='w-[22rem] h-36 flex items-center relative gap-3 my-4 overflow-hidden md:w-[35rem] lg:w-[43rem]'>
       {content.imageLinks ? (
         <img
           src={content.imageLinks.thumbnail}
           alt={content.title}
-          className=' w-[7.5rem]'
+          className='w-[6rem] md:w-[6.5rem] lg:w-[7.5rem]'
         />
       ) : (
         <img
           src={cameraImage}
           alt='No image'
-          className='opacity-40 w-[7.5rem]'
+          className='opacity-40 w-[6rem] md:w-[6.5rem] lg:w-[7.5rem]'
         />
       )}
 
       <div>
-        <h1 className='font-bold text-xl'>{content.title}</h1>
+        <h1 className='font-bold text-base md:text-xl'>{content.title}</h1>
 
         <ul className='my-2 text-shadowText-100'>
           <li className='flex items-center gap-2 mb-1'>
-            <User weight='bold' />
-            <span>{content.authors}</span>
+            <User weight='bold' className='text-sm md:text-base' />
+            <span className='text-sm md:text-base'>{content.authors}</span>
           </li>
 
           <li className='flex items-center gap-2 mb-1'>
-            <Calendar weight='bold' />
-            <span>{content.publishedDate}</span>
+            <Calendar weight='bold' className='text-sm md:text-base' />
+            <span className='text-sm md:text-base'>
+              {content.publishedDate}
+            </span>
           </li>
 
           {content.infoLink && (
             <li className='flex items-center gap-2'>
-              <Link weight='bold' />
-              <a href={content.infoLink} target='_blank'>
+              <Link weight='bold' className='text-sm md:text-base' />
+              <a
+                href={content.infoLink}
+                className='text-sm md:text-base'
+                target='_blank'
+              >
                 See more
               </a>
             </li>
--- src/hooks/useSearch.tsx
+++ src/hooks/useSearch.tsx
@@ -1,6 +1,6 @@
 import { createContext, ReactNode, useContext, useState } from 'react';
 import { api } from '../services/api';
-import { authorsFormatter } from '../util/helpers';
+import { authorsFormatter, dateFormatter } from '../util/helpers';
 
 interface SearchResults {
   id: string;
@@ -45,7 +45,9 @@ export function SearchProvider({ children }: SearchProviderProps) {
           subtitle: result.volumeInfo.subtitle,
           authors: authorsFormatter(result.volumeInfo.authors),
           publishedDate: result.volumeInfo.publishedDate
-            ? result.volumeInfo.publishedDate
+            ? dateFormatter.format(
+                new Date(`${result.volumeInfo.publishedDate} 00:00:00`)
+              )
             : 'N/A',
           pageCount: result.volumeInfo.pageCount,
           imageLinks: result.volumeInfo.imageLinks,
